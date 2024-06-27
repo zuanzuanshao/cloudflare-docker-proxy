4275ddecb36066c85378f5634b47e83f73fd2177
ZuanZuan <shaozuanzuan@gmail.com>
2024-06-27 13:58:45 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,19 +5,23 @@ addEventListener("fetch", (event) => {
 
 const dockerHub = "https://registry-1.docker.io";
 
-const routes = {
-  // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+// const routes = {
+//   // production
+//   "docker.libcuda.so": dockerHub,
+//   "quay.libcuda.so": "https://quay.io",
+//   "gcr.libcuda.so": "https://gcr.io",
+//   "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
+//   "k8s.libcuda.so": "https://registry.k8s.io",
+//   "ghcr.libcuda.so": "https://ghcr.io",
+//   "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+//   "ecr.libcuda.so": "https://public.ecr.aws",
 
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+//   // staging
+//   "docker-staging.libcuda.so": dockerHub,
+// };
+
+const routes = {
+  "${workername}.${username}.workers.dev/": "https://registry-1.docker.io",
 };
 
 function routeByHosts(host) {
